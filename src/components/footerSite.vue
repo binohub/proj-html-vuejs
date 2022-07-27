3b4c21e6afbee63e3e2bffdc543d5f2471f78a0b
binohub <yeebino001@gmail.com>
2022-07-27 02:30:33 +0200

    footerSite V 0.1

--- src/components/footerSite.vue
+++ src/components/footerSite.vue
@@ -1,16 +1,62 @@
 <template>
-    <div>
+    <div id="footerSite">
+        <div class="container">
+            <div class="row">
+
+                <div id="topFooter ">
+                    <div id="textFooter" class="col-2 py-5">
+                        <img class="img-fluid" src="../assets/img/nexgen-logo-web.png" alt="logo">
+                        <span>A Functional HTML Template For Corporate & Business.</span>
+                        <div id="topHeaderNumber" class="my-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                                class="bi bi-telephone-fill" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd"
+                                    d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
+                            </svg>
+                            <span class="mx-2">+1 (305) 1234-5678</span>
+                        </div>
+                        <div id="topHeaderMail" class="">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                                class="bi bi-envelope-fill" viewBox="0 0 16 16">
+                                <path
+                                    d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z" />
+                            </svg>
+                            <span class="mx-2">hello@example.com</span>
+                        </div>
+                        <button type="button" class="btn btn-outline-success fw-bold my-2">GET IN TOUCH</button>
+                    </div>
+                </div>
+
+                <div id="bottomFooter" class="d-flex justify-content-between align-items-center">
+                    <span class="text-left">Enjoy the low price. We are tracking any intentions of privacy</span>
+                    <span class="text-right">© 2020 NEXGEN is Proudly Powered by <a href="#">Codings</a></span>
+                </div>
+
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'footerSite',
-    props: {
-    }
 }
 </script>
 
 
 <style scoped lang="scss">
+#footerSite {
+    background-color: black;
+    color: white;
+
+    #topFooter {
+        background-color: antiquewhite; 
+        #textFooter {
+        }
+    }
+
+    #bottomFooter {
+        height: 5vh;
+        font-size: .8rem;
+    }
+}
 </style>
