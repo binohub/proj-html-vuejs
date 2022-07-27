6bb47d15f758f014fa2e2aa903c3acba394d92dd
binohub <yeebino001@gmail.com>
2022-07-27 17:12:14 +0200

    sectionLogos added

--- src/components/mainSite.vue
+++ src/components/mainSite.vue
@@ -2,16 +2,19 @@
   <div id="mainSite">
     <sectionServices />
     <sectionCompany />
+    <sectionLogos />
   </div>
 </template>
 
 <script>
 import sectionServices from "../components/sectionServices.vue"
 import sectionCompany from "../components/sectionCompany.vue"
+import sectionLogos from "../components/sectionLogos.vue"
 export default {
   components:{
     sectionServices,
     sectionCompany,
+    sectionLogos,
   }
 }
 </script>
---
+++ src/components/sectionLogos.vue
@@ -0,0 +1,52 @@
+<template>
+
+    <div id="sectionLogos" class="d-flex align-items-center">
+        <div class="container">
+            <div class="row justify-content-between">
+
+                <div class="divLogo" v-for="(item, index) in logoList" :key="index">
+                    <img class="img-fluid" src="../assets/img/logo-1.png" alt="logo">
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+
+export default {
+    data: function () {
+        return {
+            logoList: [
+                {
+                    logoImg: "logo-1.png",
+                },
+                {
+                    logoImg: "logo-2.png",
+                },
+                {
+                    logoImg: "logo-3.png",
+                },
+                {
+                    logoImg: "logo-4.png",
+                },
+                {
+                    logoImg: "logo-5.png",
+                },
+            ],
+        }
+    },
+}
+</script>
+
+
+<style scoped lang="scss">
+#sectionLogos {
+    height: 20vh;
+    .divLogo{
+        width: 200px;
+    }
+}
+</style>
