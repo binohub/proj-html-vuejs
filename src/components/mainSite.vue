e09316a312aec7dfcbbc9337b217725b323dcb33
binohub <yeebino001@gmail.com>
2022-07-28 04:21:22 +0200

    sectionKnow added

--- src/components/mainSite.vue
+++ src/components/mainSite.vue
@@ -4,6 +4,7 @@
     <sectionCompany />
     <sectionLogos />
     <sectionNews />
+    <sectionKnow />
   </div>
 </template>
 
@@ -12,12 +13,14 @@ import sectionServices from "../components/sectionServices.vue"
 import sectionCompany from "../components/sectionCompany.vue"
 import sectionLogos from "../components/sectionLogos.vue"
 import sectionNews from "../components/sectionNews.vue"
+import sectionKnow from "../components/sectionKnow.vue"
 export default {
   components:{
     sectionServices,
     sectionCompany,
     sectionLogos,
     sectionNews,
+    sectionKnow,
   }
 }
 </script>
---
+++ src/components/sectionKnow.vue
@@ -0,0 +1,47 @@
+<template>
+
+    <div id="sectionKnow">
+        <div class="container">
+            <div class="row justify-content-between py-5">
+
+                <div class="w-50">
+                    <span>NEWSLETTER</span>
+                    <h2>Know First</h2>
+                    <span>Follow closely and receive content about our company and the news of the current
+                        market.</span>
+                </div>
+                <div class="input&btn w-50">
+                    <div class="form-outline my-3">
+                        <input type="text" id="form1" class="form-control" placeholder="Name" aria-label="Search" />
+                    </div>
+                    <div class="form-outline my-3">
+                        <input type="mail" id="form1" class="form-control" placeholder="Email" aria-label="Search" />
+                    </div>
+                    <button id="btnGreen" type="button" class="btn">SUBSCRIBE</button>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+</script>
+
+
+<style scoped lang="scss">
+#sectionKnow{
+    background-image: url("../assets/img/bg-1.jpg");
+    padding: 8rem 0;
+        input{
+            border-radius: 0;
+            background: rgb(255, 255, 255, .7)
+        }
+        button{
+            border-radius: 0;
+            background-color: green;
+            color: white;
+        }
+}
+</style>
